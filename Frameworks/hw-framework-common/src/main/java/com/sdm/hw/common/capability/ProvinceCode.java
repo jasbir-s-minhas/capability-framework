68cbed99a13fd0927245f892d3d2c70cf0d76149
Jasbir Minhas <jasbir.s.minhas@gmail.com>
2017-10-16 13:11:19 -0400

    Created.

---
+++ Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/ProvinceCode.java
@@ -0,0 +1,48 @@
+package com.sdm.hw.common.capability;
+/**
+ * This enum representing ProvinceCode Code:
+ *
+ * @author Jasbir Minhas
+ * @version 1.0
+ * @since 2017-10-10
+ */
+
+public enum ProvinceCode implements CapabilityKey{
+	Alberta("AB"),
+	BritishColumbia("BC"),
+	Manitoba("MB"),
+	NewBrunswick("NB"),
+	NewfoundlandAndLabrador("NL"),
+	NorthwestTerritories("NT"),
+	NovaScotia("NS"),
+	Nunavut("NU"),
+	Ontario("ON"),
+	PrinceEdwardIsland("PE"),
+	Quebec("QC"),
+	Saskatchewan("SK"),
+	Yukon("YT");
+
+
+	/**
+     *  a String representing a path to the capability
+     */
+    private final String provinceCode;
+
+    /**
+     * constructor for Enum
+	 *
+     * @param provinceCode String representing the province code.
+     *
+     */
+    ProvinceCode(final String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    /**
+     * @see Enum#toString()
+     */
+    @Override
+    public String toString() {
+        return provinceCode;
+    }
+}
