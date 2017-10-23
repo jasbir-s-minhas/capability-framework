53ab161108d586eee22a4c3e1a9708df29f4bfac
Jasbir Minhas <jasbir.s.minhas@gmail.com>
2017-10-23 16:52:51 -0400

    Added a String type capability

--- Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/CapabilityStringKey.java
+++ Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/CapabilityStringKey.java
@@ -40,7 +40,9 @@ package com.sdm.hw.common.capability;
 
 public enum CapabilityStringKey implements CapabilityKey {
     // the following enum is for testing multilevel of groups with float capability during development process.
-    TEST_2_LEVEL_GROUP("testGroup1L1.testGroup1L2.testStringCapability");
+    TEST_2_LEVEL_GROUP("testGroup1L1.testGroup1L2.testStringCapability"),
+    //DECN Business entity type value
+    DECN_BUSINESS_ENTITY_TYPE("eHealth.decnBusinessEntityType");
 
     /**
      * a String representing a path to the capability
